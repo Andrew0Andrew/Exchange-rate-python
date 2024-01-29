539b6a323572c8e3b8581b28a985105474b2b477
Andrew <137941040+Andrew0Andrew@users.noreply.github.com>
2024-01-29 19:24:47 +0300

    Created exchange rate using python and API

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import requests
 
-def get_currency():
-    url = 'https://currate.ru/api/?get=currency_list&key=YOUR-API-KEY'
+currency = input('write currency - ').upper()
+comparable_currency = input('write comparable currency - ').upper()
+def exchangeRate(currency, comparable_currency):
+    url = f'https://v6.exchangerate-api.com/v6/c1a3397367a9dcdcbdf3c622/latest/{currency}'
+    response = requests.get(url).json()
+    data = round(response["conversion_rates"][comparable_currency],2)
+    return data
+
+result = exchangeRate(currency, comparable_currency)
+print(f' 1 {currency} - {result} {comparable_currency}')
+
+
+
 
